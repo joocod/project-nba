982349bd9ba4866df319e15797e5b587b6266862
joocod <20634hdj@gmail.com>
2024-01-05 19:39:57 +0900

    commit 01/05

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import Search from './component/Search';
 import Mainvideo from './pages/Mainvideo';
 import Descrip from './component/Descrip';
 import Community from './pages/Community';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
       <>
-        <GlobalStyle/>
-        <Main/>
-        <Search/>
-        <Mainvideo/>
-        <Descrip/>
-        <Community/>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle/>
+            <Main/>
+              <Search/>
+              <Mainvideo/>
+            <Descrip/>
+          <Community/>
+        </QueryClientProvider>
       </>
   );
 }
